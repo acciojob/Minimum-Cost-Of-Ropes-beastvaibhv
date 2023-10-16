21f03a1def2a8ad5e6b37cd1a798de7d2e67cd99
Vaibhav Bisht <92950515+beastvaibhv@users.noreply.github.com>
2023-10-16 16:08:35 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,1 +1,20 @@
 
+function calculateMinCost() {
+	const text = document.getElementById("rope-lengths");
+	const result = document.getElementById("result");
+	const ropeLengths = text.value.split(",").map(Number);
+    const minimumCost = findMinCost(ropeLengths);
+    result.textContent =   minimumCost;
+	return;
+	
+}
+function findMinCost(arr) {
+        let minCost = 0;
+        while (arr.length > 1) {
+          arr.sort((a, b) => a - b);
+          const sum = arr[0] + arr[1];
+          minCost += sum;
+          arr.splice(0, 2, sum);
+        }
+        return minCost;
+      }
