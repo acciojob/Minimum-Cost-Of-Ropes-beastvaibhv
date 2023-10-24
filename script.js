265499afaf33caf6bfade43b8e96e0cac5b346ac
Vaibhav Bisht <92950515+beastvaibhv@users.noreply.github.com>
2023-10-24 11:54:15 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,20 +1,27 @@
 
-function calculateMinCost() {
+	
+	
+
+myButton = document.getElementById("calculate").addEventListener("click", findMinCost);
+
+function findMinCost() {
 	const textElement = document.getElementById("rope-lengths");
 	const resultElement = document.getElementById("result");
-	const ropeLengths = textElement.value.split(",").map(Number);
-    const minimumCost = findMinCost(ropeLengths);
-    resultElement.textContent =   minimumCost;
-	return;
-	
+	const arr = textElement.value.split(",").map(Number);
+    const ropeLengths = textElement.value.trim();
+    if (!ropeLengths) {
+        resultElement.textContent = "Please enter rope lengths.";
+        return;
+    }
+
+    
+
+    let minCost = 0;
+    while (arr.length > 1) {
+        arr.sort((a, b) => a - b);
+        const sum = arr[0] + arr[1];
+        minCost += sum;
+        arr.splice(0, 2, sum);
+    }
+    resultElement.textContent = minCost;
 }
-function findMinCost(arr) {
-        let minCost = 0;
-        while (arr.length > 1) {
-          arr.sort((a, b) => a - b);
-          const sum = arr[0] + arr[1];
-          minCost += sum;
-          arr.splice(0, 2, sum);
-        }
-        return minCost;
-      }
